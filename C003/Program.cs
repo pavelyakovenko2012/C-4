08e80040d103d240d8d2766cecaa0d8e75f30054
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-08-03 17:18:43 +0500

    003

---
+++ C003/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+int number = Convert.ToInt32(Console.ReadLine());
+string numstr = Convert.ToString(number);
+System.Console.WriteLine(numstr.Length);
