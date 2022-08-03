f5bca85391f7b058dfbc8272e33ece6743a2ca88
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-08-03 17:21:12 +0500

    008

---
+++ C008/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+int[] array = GetBinaryArray(8);
+System.Console.Write($"[{String.Join(",", array)}]");
+int[] GetBinaryArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(2);
+    }
+    return result;
+}
