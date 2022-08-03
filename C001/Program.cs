de85924aa12b69e87135c7a4cee28f9853ff5ff5
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-08-03 17:17:19 +0500

    001

---
+++ C001/Program.cs
@@ -0,0 +1,12 @@
+﻿void SalarySpecialist(int number);
+{
+    number = 4*number;
+    System.Console.WriteLine(number);
+}
+SalarySpecialist(5);
+
+int GetSalary(int number);
+{
+    return 4*number
+}
+System.Console.WriteLine(GetSalary(5));
