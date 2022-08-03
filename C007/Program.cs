38de2e5ffb7965e4b5331badc723d23287a28bbf
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-08-03 17:20:40 +0500

    007

---
+++ C007/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+int inputNumber = Convert.ToInt32(System.Console.ReadLine());
+int result = 1;
+for (int start = 1; start <= inputNumber; start++)
+{
+    result*=start;
+}
+System.Console.WriteLine(result);
