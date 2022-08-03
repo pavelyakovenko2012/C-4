69edf078d6921c444c6e0d569976290a7290184d
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-08-03 17:19:39 +0500

    005

---
+++ C005/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(System.Console.ReadLine());
+int countPositive = 0;
+while(number !=0)
+{
+    number/=10;
+    countPositive++;
+}
+System.Console.WriteLine($"Количество чисел: {countPositive}");
