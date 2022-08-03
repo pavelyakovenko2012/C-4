1147fdebcd882a4d82a59a311373f9509914e404
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-08-03 17:18:03 +0500

    002

---
+++ C002/Program.cs
@@ -0,0 +1,14 @@
+﻿Напишите программу,
+// которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i <= number; i++)
+{
+    sum = sum + i;
+}
+// System.Console.WriteLine("Сумма элементов от 1 до" + number + "=" + sum);
+System.Console.WriteLine($"Сумма элементов от 1 до {number} = {sum}");
